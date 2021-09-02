2a3e8f4d71139ea305675eb81110c5e714ee4618
hokkyss <hokkyss2@gmail.com>
2021-09-02 11:00:57 +0700

    add affine encryption

--- src/main/java/com/kripto2021/hokkinon/affine/Affine.java
+++ src/main/java/com/kripto2021/hokkinon/affine/Affine.java
@@ -5,10 +5,45 @@
  */
 package com.kripto2021.hokkinon.affine;
 
+import utils.Utils;
 /**
  *
  * @author PERSONAL
  */
 public class Affine {
+    private long b;
+    private long m;
+    private final long n = 26; // 25252525
     
+    public Affine(long b, long m) {
+        this.b = b;
+        this.m = m;
+    }
+    
+    public static long[] getKeyFrom(String text) {
+        String[] splitted = text.split(" ", 2);
+        long b = Long.parseLong(splitted[0]);
+        long m = Long.parseLong(splitted[1]);
+        
+        long[] result = new long[2];
+        result[0] = b;
+        result[1] = m;
+        return result;
+    }
+    
+    private char encode(char oneChar) {
+        char c;
+        
+        return (char) ((char)((this.m * ((int)oneChar - 65)) % 26) + 65);
+    }
+    
+    public String encrypt(String text) {
+        char[] each = Utils.cleanString(text).toCharArray();
+        StringBuilder builder = new StringBuilder();
+        
+        for(char c: each) {
+            builder.append(encode(c));
+        }
+        return builder.toString();
+    }
 }
---
+++ src/main/java/utils/Utils.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+/**
+ *
+ * @author PERSONAL
+ */
+public class Utils {
+    public static long GCD(long a, long b) {
+        if (a == 0) return b;
+        return GCD(b % a, a);
+    }
+    /**
+     * Counts a^-1 (mod m) using Bezout Identity
+     * @see https://www.dcode.fr/bezout-identity
+     * @param a
+     * @param m
+     * @return 
+     */
+    public static long moduloInverse(long a, long m) {
+        assert(GCD(a, m) == 1);
+
+        long r = a, r_ = m, u = 1, v = 0, u_ = 0, v_ = 1;
+        while (r_ != 0) {
+            long q = (long)r / r_;
+            long tempr = r, tempu = u, tempv = v;
+            r = r_; u = u_; v = v_;
+            r_ = tempr - (q * r_); u_ = tempu - (q * u_); v_ = tempv - (q * v_);
+        }
+        // return r, u, v
+        // r = GCD(a, m) = 1
+        // v = a number such that au + mv = r
+        return u;
+    }
+    
+    public static String cleanString(String s) {
+        String caps = s.toUpperCase();
+        StringBuilder builder = new StringBuilder();
+        
+        for(char c: caps.toCharArray()){
+            if ((int)c >= 65 && (int)c <= 90) {
+                builder.append(c);
+            }
+        }
+        return builder.toString();
+    }
+}
